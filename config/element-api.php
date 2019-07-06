00d2e682f4b26f6f2a1080aaa88e9de6b489086d
Josef Renner <josef.renner@gmail.com>
2019-07-06 14:09:11 +0200

    fix api

--- config/element-api.php
+++ config/element-api.php
@@ -1,27 +1,68 @@
 <?php
-use craft\elements\Entry;
+use craft\elements\Asset;
 use craft\helpers\UrlHelper;
+use craft\elements\Category;
+use craft\elements\Entry;
 
 return [
   'endpoints' => [
-    'posts.json' => function() {
+    'api/paintings.json' => [
+      'resourceKey' => 'paintings',
+      'elementType' => Asset::class,
+      'criteria' => ['volume' => 'paintings'],
+      'transformer' => function(Asset $asset) {
+        return [
+          'id' => $asset->id,
+          'dateCreated' => $asset->dateCreated,
+          'filename' => $asset->filename,
+          'title' => $asset->title,
+          'type' => $asset->picType->one()->title,
+          'typeId' => $asset->picType->one()->id,
+          'imgUrl' => $asset->url,
+          'jsonUrl' => UrlHelper::url("api/painting/{$asset->id}.json"),
+          'categoryUrl' => UrlHelper::url("api/category/{$asset->picType->one()->id}.json"),
+        ];
+      }
+    ],
+    'api/painting/<assetId:\d+>.json' => function($assetId) {
       return [
-        'elementType' => Entry::class,
-        'criteria' => ['section' => 'blog', 'limit' => '25', 'orderBy' => 'postData desc'],
-        'elementsPerPage' => 25,
-        'transformer' => function(Entry $entry) {
+        'resourceKey' => 'painting',
+        'elementType' => Asset::class,
+        'criteria' => ['volume' => 'paintings', 'id' => $assetId],
+        'transformer' => function(Asset $asset) {
           return [
-            'id' => $entry->id,
-            'title' => $entry->title,
-            'slug' => $entry->slug,
-            'data' => $entry->postDate,
-            'body' => $entry->body,
-            'externalUrl' => $entry->externalUrl
+            'id' => $asset->id,
+            'filename' => $asset->filename,
+            'dateCreated' => $asset->dateCreated,
+            'title' => $asset->title,
+            'width' => $asset->picWidth,
+            'height' => $asset->picHeight,
+            'technique' => $asset->picTechnique,
+            'type' => $asset->picType->one()->title,
+            'typeId' => $asset->picType->one()->id,
+            'imgUrl' => $asset->url,
+            'categoryUrl' => UrlHelper::url("api/category/{$asset->picType->one()->id}.json"),
           ];
         }
       ];
-    }
+    },
+    'api/category/<categoryId:\d+>.json' => function($categoryId) {
+      return [
+        'resourceKey' => 'paintingsPerCategory',
+        'elementType' => Asset::class,
+        'criteria' => ['volume' => 'paintings', 'relatedTo' => $categoryId],
+        'transformer' => function(Asset $asset) {
+          return [
+            'id' => $asset->id,
+            'dateCreated' => $asset->dateCreated,
+            'filename' => $asset->filename,
+            'title' => $asset->title,
+            'type' => $asset->picType->one()->title,
+            'imgUrl' => $asset->url,
+            "jsonUrl" => UrlHelper::url("api/painting/{$asset->id}.json"),
+          ];
+        } 
+      ];
+    } 
   ]
 ];
-
-?>
